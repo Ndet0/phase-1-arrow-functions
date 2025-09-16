e46b2e2868f909d6c4f8785b8f850a67f3fcfce4
Ndet0 <festusndet00@gmail.com>
2025-09-16 09:08:40 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a, b) {
+  return a / b;
+}
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
+
+
+divide(4, 2);
+square(5);
+add(3, 7);   
+
+console.log(square(6));
+console.log(add(10, 15));
+console.log(divide(20, 4));
